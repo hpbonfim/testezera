ee4c443de05e0e83c5c454c2a77b8e37516c7bd9
Henrique Paulo Bonfim <40275173+hpbonfim@users.noreply.github.com>
2022-10-11 07:29:25 +0000

    add mock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 import useSWR from 'swr'
 import { Layout } from '../components/Layout'
 import { PersonComponent } from '../components/Person'
@@ -6,9 +7,17 @@ import { Person } from '../components/Person/types'
 
 const Home: NextPage = () => {
   const { data, error } = useSWR('/api/people', (url: string) => fetch(url).then((res) => res.json()))
+  const [dataTable, setDataTable] = useState<typeof mock_people>()
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
+  useEffect(() => {
+    if (!error) {
+      if (data)
+        setDataTable(data)
+      else
+        setDataTable(mock_people)
+    } else
+      setDataTable(mock_people)
+  }, [data, error])
 
   return (
     <Layout>
@@ -20,7 +29,7 @@ const Home: NextPage = () => {
       </h1>
 
       <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-        {data.map((p: Person) => <PersonComponent key={p.id} person={p} />)}
+        {dataTable && dataTable.map((p: Person) => <PersonComponent key={p.id} person={p} />)}
       </div>
 
       <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
@@ -69,3 +78,106 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+const mock_people = [
+  {
+    id: '1',
+    name: 'Luke Skywalker',
+    height: '172',
+    mass: '77',
+    hair_color: 'blond',
+    skin_color: 'fair',
+    eye_color: 'blue',
+    gender: 'male',
+  },
+  {
+    id: '2',
+    name: 'C-3PO',
+    height: '167',
+    mass: '75',
+    hair_color: 'n/a',
+    skin_color: 'gold',
+    eye_color: 'yellow',
+    gender: 'n/a',
+  },
+  {
+    id: '3',
+    name: 'R2-D2',
+    height: '96',
+    mass: '32',
+    hair_color: 'n/a',
+    skin_color: 'white, blue',
+    eye_color: 'red',
+    gender: 'n/a',
+  },
+  {
+    id: '4',
+    name: 'Darth Vader',
+    height: '202',
+    mass: '136',
+    hair_color: 'none',
+    skin_color: 'white',
+    eye_color: 'yellow',
+    gender: 'male',
+  },
+  {
+    id: '5',
+    name: 'Leia Organa',
+    height: '150',
+    mass: '49',
+    hair_color: 'brown',
+    skin_color: 'light',
+    eye_color: 'brown',
+    gender: 'female',
+  },
+  {
+    id: '6',
+    name: 'Owen Lars',
+    height: '178',
+    mass: '120',
+    hair_color: 'brown, grey',
+    skin_color: 'light',
+    eye_color: 'blue',
+    gender: 'male',
+  },
+  {
+    id: '7',
+    name: 'Beru Whitesun Lars',
+    height: '165',
+    mass: '75',
+    hair_color: 'brown',
+    skin_color: 'light',
+    eye_color: 'blue',
+    gender: 'female',
+  },
+  {
+    id: '8',
+    name: 'R5-D4',
+    height: '97',
+    mass: '32',
+    hair_color: 'n/a',
+    skin_color: 'white, red',
+    eye_color: 'red',
+    gender: 'n/a',
+  },
+  {
+    id: '9',
+    name: 'Biggs Darklighter',
+    height: '183',
+    mass: '84',
+    hair_color: 'black',
+    skin_color: 'light',
+    eye_color: 'brown',
+    gender: 'male',
+  },
+  {
+    id: '10',
+    name: 'Obi-Wan Kenobi',
+    height: '182',
+    mass: '77',
+    hair_color: 'auburn, white',
+    skin_color: 'fair',
+    eye_color: 'blue-gray',
+    gender: 'male',
+  },
+]
