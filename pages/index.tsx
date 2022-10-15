43170e8d9522530fcb706622962997d6b77a0b79
Henrique Paulo Bonfim <40275173+hpbonfim@users.noreply.github.com>
2022-10-15 17:39:43 +0000

    change homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,22 @@
 import type { NextPage } from 'next'
-import { Map } from './map'
-import { Wrapper, Status } from "@googlemaps/react-wrapper"
-
-const render = (status: Status) => {
-  switch (status) {
-    case Status.LOADING:
-      return <div>Carregando...</div>
-    case Status.FAILURE:
-      return <div>Erro ao carregar. xD</div>
-    case Status.SUCCESS:
-      return <Map center={null} zoom={null} />
-  }
-}
 
 const Home: NextPage = () => {
-  return <Wrapper
-    apiKey={process.env.NEXT_PUBLIC_GOOGLE_API as string}
-    version='weekly'
-    region='BR'
-    language='pt'
-    render={render}
-  />
+  return (
+    <div className="mt-8 flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold">
+        <code className="rounded-md bg-gray-100 p-3 font-mono text-5xl">
+          Teste/z3rA
+        </code>
+      </h1>
+
+      <div className="mt-8 flex items-center justify-center w-full">
+        <a href="/maps" className="rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600">
+          <p className="text-xl">Acesse aqui</p>
+          <h3 className="text-2xl font-bold">Google Maps &rarr;</h3>
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default Home
